cf8ca1e93e5938048265bc617b16cc04f479d7c2
cward85 <camiloward@gmail.com>
2026-06-15 15:52:14 -0400

    Updated pages that are no longer needed.  Added examples to portfolio

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -11,36 +11,23 @@ namespace PersonalWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+        {            
             return View();
         }
 
         public ActionResult Portfolio()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
         public ActionResult Resume()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Blog()
+        {         
             return View();
         }
 
