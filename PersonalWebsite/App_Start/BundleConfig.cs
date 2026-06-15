f2a07a55d3fc50d18438057b8111d7a2bbc6958c
cward85 <camiloward@gmail.com>
2026-06-15 14:32:23 -0400

    Add project files.

---
+++ PersonalWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PersonalWebsite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                     //             "~/Scripts/dist/js/bootstrap.bundle.min.js",
+                     "~/Scripts/dist/js/coreui.bundle.min.js"
+                     ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Scripts/dist/css/themes/bootstrap/bootstrap.min.css",
+
+                "~/Content/bootstrap-icons-1.13.1/bootstrap-icons.css",
+                "~/Scripts/dist/css/coreui.min.css",
+                "~/Content/Site.css"
+                      //    "~/Content/bootstrap.css",
+
+                      //"~/Content/jquery.ui.css",
+                      //"~/Content/jquery.ui.theme.css",
+                      //"~/Content/jquery.ui.structure.css"
+                      ));
+        }
+    }
+}
---
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ResumeDownload()
+        {
+            string path = Path.Combine(Server.MapPath("~/Content/Resume/"), "Camilo Ward - Resume 5_2026.pdf");
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            string contentType = MimeMapping.GetMimeMapping(path);
+
+            return File(bytes, contentType, "Camilo Ward - Current Resume.pdf");
+        }
+    }
+}
