83d13faead0fa9e7fe34df791bb9aae647f92fcf
cward85 <camiloward@gmail.com>
2026-06-30 23:39:26 -0400

    Added additional meta tags for indexing purposes

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
@@ -12,12 +14,16 @@ namespace PersonalWebsite.Controllers
     public class HomeController : Controller
     {
         public ActionResult About()
-        {            
+        {
+            ViewBag.MetaDescription = "About Camilo Ward and a summary of his career and his plans for Freelance work and hobbies.";
+            ViewBag.MetaKeywords = "Camilo, Ward, freelance, unity, articy";
             return View();
         }
 
         public ActionResult Portfolio()
         {
+            ViewBag.MetaDescription = "Camilo Ward's current freelance portfolio from websites built with C#, MVC, SQL Server, javascript, jquery, and bootstrap.";
+            ViewBag.MetaKeywords = "Camilo, Ward, freelance, C#, mvc, SQL, javascript, jquery, bootstrap, portfolio";
             return View();
         }
 
@@ -31,6 +37,13 @@ namespace PersonalWebsite.Controllers
             return View();
         }
 
+        public ActionResult Freelance()
+        {
+            ViewBag.MetaDescription = "What Camilo Ward offers potential clients that want to use his freelance services.";
+            ViewBag.MetaKeywords = "Camilo, Ward, freelance, services";
+            return View();
+        }
+
         public ActionResult ResumeDownload()
         {
             string path = Path.Combine(Server.MapPath("~/Content/Resume/"), "Camilo Ward - Resume 5_2026.pdf");
@@ -39,6 +52,6 @@ namespace PersonalWebsite.Controllers
             string contentType = MimeMapping.GetMimeMapping(path);
 
             return File(bytes, contentType, "Camilo Ward - Current Resume.pdf");
-        }
+        }        
     }
 }
